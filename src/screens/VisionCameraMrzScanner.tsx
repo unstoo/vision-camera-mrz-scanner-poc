69614f4538053003001bd20e375dae14b0d49147
unstoo <unstoo@gmail.com>
2024-10-21 12:40:50 +0300

    setup scanner package, update it to current vision-camera version

---
+++ src/screens/VisionCameraMrzScanner.tsx
@@ -0,0 +1,30 @@
+
+import * as React from 'react';
+
+import { View, StyleSheet } from 'react-native';
+import {MRZProperties, MRZScanner} from 'vision-camera-mrz-scanner';
+
+export default function VisionCameraMrzScanner() {
+  return (
+    <View style={styles.container}>
+      <MRZScanner
+        mrzFinalResults={(mrzResults: MRZProperties) => {
+          // do something with the results
+          console.log('mrzResults: ', JSON.stringify(mrzResults, null, 2));
+        }}
+        enableMRZFeedBack={true}
+        enableBoundingBox={false}
+        style={StyleSheet.absoluteFill}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      height: '100%',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
